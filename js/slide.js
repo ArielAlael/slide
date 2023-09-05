69d6d0f3c4d638e22542a4e29dd759854fe9d6ec
Ariel <arielalael@gmail.com>
2023-09-05 09:00:03 -0300

    calculo de distancias quando os eventos ocorrem

--- js/slide.js
+++ js/slide.js
@@ -1,37 +1,50 @@
 export default class Slide {
     constructor(slide, wrapper) {
         this.slide = document.querySelector(slide)
-        this.wrapper = document.querySelector(wrapper)
+        this.wrapper = document.querySelector(wrapper);
+        this.dist = { finalPosition: 0, startX: 0, movement: 0 }
+    }
+
+    moveSlide(distX) {
+        this.dist.movePosition = distX;
+        this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+    }
+
+    updatePosition(clientX) {
+        this.dist.movement = (this.dist.startX - clientX) * 1.6;
+        return this.dist.finalPosition - this.dist.movement;
     }
 
     onStart(event) {
-        event.preventDefault()
-        this.wrapper.addEventListener('mousedown', this.onMove)
+        event.preventDefault();
+        this.dist.startX = event.clientX;
+        this.wrapper.addEventListener('mousemove', this.onMove);
     }
 
     onMove(event) {
-        
+        const finalPosition = this.updatePosition(event.clientX);
+        this.moveSlide(finalPosition);
     }
 
     onEnd(event) {
-        this.wrapper.removeEventListener('mousemove', this.onMove)
+        this.wrapper.removeEventListener('mousemove', this.onMove);
+        this.dist.finalPosition = this.dist.movePosition;
     }
 
     addSlideEvents() {
-        this.wrapper.addEventListener('mousedown', this.onStart)
-        this.wrapper.addEventListener('mouseup', this.onEnd)
+        this.wrapper.addEventListener('mousedown', this.onStart);
+        this.wrapper.addEventListener('mouseup', this.onEnd);
     }
 
     bindEvents() {
-        this.onStart = this.onStart.bind(this)
-        this.onMove = this.onMove.bind(this)
-        this.onEnd = this.onEnd.bind(this)
+        this.onStart = this.onStart.bind(this);
+        this.onMove = this.onMove.bind(this);
+        this.onEnd = this.onEnd.bind(this);
     }
 
     init() {
-        this.bindEvents()
-        this.addSlideEvents()
-        return this
+        this.bindEvents();
+        this.addSlideEvents();
+        return this;
     }
-
 }
